1907706a8f94e156a90a148bf12b69f1311df48e
gunnuwatts <gungunwatts096@gmail.com>
2024-06-06 18:32:21 +0530

    add html

--- types.js
+++ types.js
@@ -21,3 +21,12 @@ pizza`;
 // hello2 = hello2 + ' Nice to meet you';
 
 const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old`;
+
+const html = `
+ <div>
+    <h2>${name}</h2>
+    <p>${hello}</p>
+ </div>
+ `;
+ 
+ console.log(html);

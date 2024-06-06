594c9d6907c15dd6ef959642e4d0bf6ce8541359
gunnuwatts <gungunwatts096@gmail.com>
2024-06-06 18:06:37 +0530

    add back tick

--- types.js
+++ types.js
@@ -14,8 +14,10 @@ I like
 
 pizza`;
 
-const hello = 'hello my name is' + name + ". Nice to meet you";
+// const hello = 'hello my name is ' + name + ". Nice to meet you";
 
-let hello2 = 'hello my name is ';
-hello2 = hello2 + name;
-hello2 = hello2 + ' Nice to meet you';
+// let hello2 = 'hello my name is ';
+// hello2 = hello2 + name;
+// hello2 = hello2 + ' Nice to meet you';
+
+const hello = `hello my name is ${name}. Nice to meet you`;

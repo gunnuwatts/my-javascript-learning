78d7c52313bb9a3854a046515a9c67f49ffd637c
gunnuwatts <gungunwatts096@gmail.com>
2024-06-13 21:20:26 +0530

    add argument

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -2,6 +2,8 @@
 function calculateBill() {
     // this is the function body
     console.log('Running Calculate bill!!');
+    const total = 100 * 1.13;
+    console.log(total);
 }
 
 // Function call. or **Run**

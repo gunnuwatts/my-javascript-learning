81046bc2e28b8cfefec49dd89fa851c98c1a65f4
gunnuwatts <gungunwatts096@gmail.com>
2024-06-13 20:42:33 +0530

    add Calculatebill

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,1 +1,8 @@
-console.log('it works!');
+// Function Definition
+function calculateBill() {
+    // this is the function body
+    console.log('Running Calculate bill!!');
+}
+
+// Function call. or **Run**
+calculateBill();

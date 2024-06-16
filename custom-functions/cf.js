009b52e3c271543ae501ea90db919630068f90af
gunnuwatts <gungunwatts096@gmail.com>
2024-06-16 21:40:53 +0530

    add parameters

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,18 +1,12 @@
 // Function Definition
-let bill = 100;
-const taxRate = 0.13;
-
-function calculateBill() {
+function calculateBill(billAmount, taxRate) {
     // this is the function body
     console.log('Running Calculate Bill!!');
-
-    const total = bill * 1 + taxRate;
+    const total = billAmount * (1 + taxRate);
     return total;
 }
 
 // Function call. or **Run**
-const myTotal = calculateBill();
-bill = 200;
-const myTotal2 = calculateBill();
-
+const myTotal = calculateBill(100, 0.13);
+const myTotal2 = calculateBill(200, 0.13);
 console.log(myTotal, myTotal2);

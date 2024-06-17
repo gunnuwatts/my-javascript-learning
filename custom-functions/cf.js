3e0a0b51de88243802d751ec6b3f58bf46ea959d
gunnuwatts <gungunwatts096@gmail.com>
2024-06-17 23:51:12 +0530

    add changes

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,6 +1,5 @@
 // Function Definition
 function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
-    console.log(billAmount, taxRate);
     // this is the function body
     console.log('Running Calculate Bill!!');
     const total = billAmount + billAmount * taxRate + billAmount * tipRate;
@@ -27,3 +26,6 @@ function doctorize(name) {
 function yell(name = '') {
     return `HEY ${name.toUpperCase()}`;
 }
+
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);

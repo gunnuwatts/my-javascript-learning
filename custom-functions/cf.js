62a0babc4297d66dc7c88eb916aaba205f245883
gunnuwatts <gungunwatts096@gmail.com>
2024-06-17 22:39:02 +0530

    add another exampe

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -10,4 +10,12 @@ function calculateBill(billAmount, taxRate) {
 // Function call. or **Run**
 const wesTotal = 500;
 const wesTaxRate = 0.3;
-const myTotal = calculateBill(wesTotal, wesTaxRate);
+// const myTotal = calculateBill(wesTotal, wesTaxRate);
+
+const firstName = 'wes';
+function sayHiTo() {
+    return `hello ${firstName}`;
+}
+
+const greeting = sayHiTo();
+console.log(greeting);

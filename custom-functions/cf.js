18a0e422042a0c813d63fad4d5c6caa50a15327a
gunnuwatts <gungunwatts096@gmail.com>
2024-06-17 21:54:09 +0530

    add console

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,5 +1,6 @@
 // Function Definition
 function calculateBill(billAmount, taxRate) {
+    console.log(billAmount, taxRate);
     // this is the function body
     console.log('Running Calculate Bill!!');
     const total = billAmount * (1 + taxRate);
@@ -8,5 +9,3 @@ function calculateBill(billAmount, taxRate) {
 
 // Function call. or **Run**
 const myTotal = calculateBill(100, 0.13);
-const myTotal2 = calculateBill(200, 0.13);
-console.log(myTotal, myTotal2);

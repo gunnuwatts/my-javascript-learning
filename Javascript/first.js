fd9b9bc47ebadcea671024f4cd72ef3035585cf0
gunnuwatts <gungunwatts096@gmail.com>
2025-04-01 21:41:28 +0530

    L1practiceques2

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,8 +1,8 @@
-const product = {
-    title: "Ball Pen",
-    rating: 4,
-    offer: 5,
-    price: 270
-};
+const profile = {
+    username: "@shradhakhapra",
+    isFollow: false,
+    followers: 123,
+    following: 4,
+    };
 
-console.log(product);
+console.log(profile);

e7f57a23210a5c9290f7216a82283ae15ab5b6bc
gunnuwatts <gungunwatts096@gmail.com>
2025-04-05 21:47:15 +0530

    Arithmneticoperator3

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,5 +1,7 @@
 //Arithmetic operators
 let a = 5;
 let b = 2;
-let c = a + b;
-console.log("a + b =", c);
+
+console.log("a = ", a, " & b = ", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);

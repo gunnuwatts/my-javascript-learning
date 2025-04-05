2306d2559be7f9c838a226fa7e8d7d52cbd75704
gunnuwatts <gungunwatts096@gmail.com>
2025-04-05 22:02:14 +0530

    unaryoperator1

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,11 +1,19 @@
 //Arithmetic operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);    //modulus operator(remainder)
+// console.log("a ** b =", a ** b);    //exponentiation(5^2)
+
+//Unary Operator
 let a = 5;
 let b = 2;
 
 console.log("a = ", a, " & b = ", b);
-console.log("a + b =", a + b);
-console.log("a - b =", a - b);
-console.log("a * b =", a * b);
-console.log("a / b =", a / b);
-console.log("a % b =", a % b);    //modulus operator(remainder)
-console.log("a ** b =", a ** b);    //exponentiation(5^2)
+a = a + 1;  //6
+console.log("a = ", a);  //6

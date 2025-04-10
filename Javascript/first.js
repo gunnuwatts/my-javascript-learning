3bbb4d415f896edc5a97b4b0805d92a31365fabb
gunnuwatts <gungunwatts096@gmail.com>
2025-04-10 22:47:28 +0530

    Logicaloperator3

--- Javascript/first.js
+++ Javascript/first.js
@@ -36,8 +36,8 @@
 let a = 6;
 let b = 5; 
 
-let cond1 = a > b; //true
-let cond2 = a === 5; //false
+let cond1 = a < b; //false
+let cond2 = a === 6; //true
 console.log("cond1 && cond2 = ", cond1 && cond2);
 
 

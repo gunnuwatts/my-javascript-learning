1290c796ec5e2137156f0b80dd6240b90a128fed
gunnuwatts <gungunwatts096@gmail.com>
2025-04-13 19:46:42 +0530

    ternaryoperator3

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,6 +1,5 @@
-//Ternary operator
+// Ternary operator
 
 let age = 16;
 
-let result = age >= 18 ? "adult" : "not adult";
-console.log(result);
+age >= 18 ? console.log("adult") : console.log("not adult");

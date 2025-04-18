4a02f9de36111bb0a775284c5ce1e4b769516d5e
gunnuwatts <gungunwatts096@gmail.com>
2025-04-18 15:16:32 +0530

    practiceq2i

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,8 +1,18 @@
-let num = prompt("enter a number:");
+//PracticeQ2
 
-if(num % 5 === 0) {
-    console.log(num, " is multiple of 5");
-} else {
-    console.log(num, "is NOT a multiple of 5");
+let score = 75;
+let grade;
+
+if (score >= 90 && score <= 100) {
+    grade = "A";
+} else if (score >= 70 && score <= 89) {
+    grade = "B";
+} else if (score >= 60 && score <= 69) {
+    grade = "C";
+} else if (score >= 50 && score <= 59) {
+    grade = "D";
+} else if (score >= 0 && score <= 49) {
+    grade = "F";
 }
-//small changes
+
+console.log("according to your scores, ypur grade was :",grade);

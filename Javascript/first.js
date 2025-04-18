13ff7a01404e24ba9b5ad2437d31977ccfc239d4
gunnuwatts <gungunwatts096@gmail.com>
2025-04-18 17:57:50 +0530

    forloop8

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,13 +1,6 @@
 //Print 1 to 5
-// for (let count = 1; count <= 5 ; count++) {
-//     console.log("Apna College"); 
-// }
-
-//Calculate sum of 1 to 5
-let sum = 0;
-let n = 100;
-for (let i = 1; i <= n; i++) {
-    sum = sum + i;
+for (let i = 1; i <= 5 ; i++) {
+    console.log("i = ", i); 
 }
-console.log("sum = ", sum);
-console.log("loop has ended");
+
+  

79791ab4c39ffc6ee285f8339010b408c3016232
gunnuwatts <gungunwatts096@gmail.com>
2025-04-18 15:29:49 +0530

    forloop1

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,5 +1,9 @@
-console.log("Apna College");
-console.log("Apna College");
-console.log("Apna College");
-console.log("Apna College");
-console.log("Apna College");
+// console.log("Apna College");
+// console.log("Apna College");
+// console.log("Apna College");
+// console.log("Apna College");
+// console.log("Apna College");
+
+for (let count = 1; count <= 5; count++) {
+    console.log("Apna College"); //5 execute
+}

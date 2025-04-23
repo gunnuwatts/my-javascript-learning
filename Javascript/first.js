d98fee50837892dc3ebfa4cdd5cd626dfb2e2491
gunnuwatts <gungunwatts096@gmail.com>
2025-04-23 23:13:43 +0530

    for-inloop1

--- Javascript/first.js
+++ Javascript/first.js
@@ -16,12 +16,25 @@
 // } while(i <= 5);
 
 //for-of loop
-let str = "Javascript";
+// let str = "Javascript";
 
-let size = 0;
-for (let val of str) { //iterator -> characters
-    console.log("val=", val);
-    size++;
+// let size = 0;
+// for (let val of str) { //iterator -> characters
+//     console.log("val=", val);
+//     size++;
+// }
+
+// console.log("string size = ", size);
+
+//for in loop
+
+let student = {
+    name: "Rahul Kumar",
+    age: 20,
+    cgpa: 7.5,
+    isPass: true,
+};
+
+for(let key in student) {
+  console.log("key=", key, "value=", student[key]);              
 }
-
-console.log("string size = ", size);

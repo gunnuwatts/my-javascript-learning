71a892f9b7308bb9e6008942cffe87434a3b1331
gunnuwatts <gungunwatts096@gmail.com>
2025-04-28 23:29:11 +0530

    createstring

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,10 +1,3 @@
-// Practice Q2
-let gameNum = 25;
-
-let userNum = prompt("Guess the game number : ");
-
-while(userNum != gameNum) { //game
-    userNum =  prompt("You entered wrong number. Guess again :")
-}
-
-console.log("congratulations, you entered the right number");
+//Strings
+let str = "ApnaCollege";  //double quotes
+let str2 = 'Shradha';     //single quotes

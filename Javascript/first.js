d756f615a050bd68133a027c40271c6e107217f2
gunnuwatts <gungunwatts096@gmail.com>
2025-04-28 22:00:44 +0530

    practiceQ1

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,40 +1,6 @@
-//Print 1 to 5
-// for (let i = 1; i <= 5 ; i++) {
-//     console.log("i = ", i); 
-// }
-
-// let i = 1;
-// while(i<=10) {
-//     console.log("Apna College");
-//     i++;
-// }
-
-// let i = 1;
-// do {
-//     console.log("i=",i);
-//     i++;  
-// } while(i <= 5);
-
-//for-of loop
-// let str = "Javascript";
-
-// let size = 0;
-// for (let val of str) { //iterator -> characters
-//     console.log("val=", val);
-//     size++;
-// }
-
-// console.log("string size = ", size);
-
-//for in loop
-
-let student = {
-    name: "Rahul Kumar",
-    age: 20,
-    cgpa: 7.5,
-    isPass: true,
-};
-
-for(let key in student) {
-  console.log("key=", key, "value=", student[key]);              
+// Practice Q1
+for(let num=0; num<=100; num++) {
+  if(num%2 === 0) { //even number
+     console.log("num=", num);
+  }
 }

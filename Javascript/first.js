bbade08c7d55bfc506ec83e1545e15ad6f9db28d
gunnuwatts <gungunwatts096@gmail.com>
2025-04-28 22:13:32 +0530

    practiceQ2

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,6 +1,10 @@
-// Practice Q1
-for(let num=0; num<=100; num++) {
-  if(num%2 !== 0) { //odd number
-     console.log("num=", num);
-  }
+// Practice Q2
+let gameNum = 25;
+
+let userNum = prompt("Guess the game number : ");
+
+while(userNum != gameNum) { //game
+    userNum =  prompt("You entered wrong number. Guess again :")
 }
+
+console.log("congratulations, you entered the right number");

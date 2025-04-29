92bf53c69bc5483cbadd6fe7d32d3ff60aef8c82
gunnuwatts <gungunwatts096@gmail.com>
2025-04-29 20:34:31 +0530

    templateliterals1

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,4 +1,8 @@
 //Strings
-let str = "ApnaCollege";  //double quotes
+// let str = "ApnaCollege";  //double quotes
 
-console.log(str[8]); //e
+// console.log(str[8]); //e
+
+//Template literals
+let specialString = `This is a template literal`;
+console.log(specialString);

e42a2851985a73a07d933cfe67bc485ea7e1c1c3
gunnuwatts <gungunwatts096@gmail.com>
2025-07-01 20:44:59 +0530

    loopingoverarray2

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,5 +1,10 @@
 let heroes = ["ironman", "thor", "hulk", "shaktiman", "spiderman", "antman"];
 //for loop
-for (let i = 0; i < heroes.length; i++) {
-    console.log(heroes[i]);
+// for (let i = 0; i < heroes.length; i++) {
+//     console.log(heroes[i]);
+// }
+
+//for of
+for (let hero of heroes) {
+    console.log(hero);
 }

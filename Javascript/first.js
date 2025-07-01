4ffce6ea171b5ca785ccaee37cf93608351fd5fa
gunnuwatts <gungunwatts096@gmail.com>
2025-07-01 19:31:27 +0530

    array3

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,3 +1,6 @@
 let marks = [97, 82, 75, 64, 36];
 console.log(marks);
 console.log(marks.length); //property
+
+let heroes = ["ironman", "thor", "hulk", "shaktiman", "spiderman"];
+console.log(heroes);

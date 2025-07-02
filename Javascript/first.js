5a6a5ea899b9e015ad80f73fa4015f787eaca729
gunnuwatts <gungunwatts096@gmail.com>
2025-07-02 13:49:26 +0530

    pracquesarray2a

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,10 +1,9 @@
-let marks = [85, 97, 44, 37, 76, 60];
+let items = [200, 645, 300, 900, 50];
 
-let sum = 0;
-
-for (let val of marks) {
-    sum += val;
+let i = 0;
+for (let val of items) {
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+    console.log(`value after offer = ${items[i]}`);
+    i++;
 }
-
-let avg = sum / marks.length;
-console.log(`avg marks of the class = ${avg}`);

0acd63042a872da7ead3d9c0ac1e9c4b54419161
gunnuwatts <gungunwatts096@gmail.com>
2025-07-02 13:52:53 +0530

    pracquesarray2b

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,9 +1,8 @@
 let items = [200, 645, 300, 900, 50];
-
-let i = 0;
-for (let val of items) {
-    let offer = val / 10;
-    items[i] = items[i] - offer;
-    console.log(`value after offer = ${items[i]}`);
-    i++;
+//for loop
+for (let i = 0; i < items.length; i++) {
+    let offer = items[i] / 10;
+    items[i] -= offer;
 }
+
+console.log(items);

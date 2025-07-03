359ed66b1be6ac1a094f1aad78fb78e28dadc63f
gunnuwatts <gungunwatts096@gmail.com>
2025-07-03 20:37:08 +0530

    arraymethods2

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,5 +1,5 @@
 let foodItems = ["potato", "apple", "litchi", "tomato"];
-
-foodItems.push("chips", "burger", "paneer");
-
 console.log(foodItems);
+let deletedItem = foodItems.pop();
+console.log(foodItems);
+console.log("deleted", deletedItem);

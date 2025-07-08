72feed8028b99f896d93eb6ec06317df044b6911
gunnuwatts <gungunwatts096@gmail.com>
2025-07-08 20:45:52 +0530

    arraymethods6

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,5 +1,6 @@
 let marvelHeroes = ["thor", "spiderman", "ironman"];
 let dcHeroes = ["superman", "batman"];
+let indianHeroes = ["shaktiman", "krish"];
 
-let heroes = marvelHeroes.concat(dcHeroes);
+let heroes = marvelHeroes.concat(dcHeroes, indianHeroes);
 console.log(heroes);

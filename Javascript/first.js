0d1bd3e1543cd86911f777d3d472b510df704045
gunnuwatts <gungunwatts096@gmail.com>
2025-07-09 20:45:04 +0530

    functioncall3

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,10 +1,9 @@
 function myFunction() {
     console.log("Welcome to Apna College!");
     console.log("We are learning JS :");
+    console.log("Welcome to Apna College!");
+    console.log("We are learning JS :");
 }
 
-myFunction();
-myFunction();
-myFunction();
 myFunction();
 myFunction();

bee4aa130f7de508b6732a2d73ffd689c5af3801
gunnuwatts <gungunwatts096@gmail.com>
2025-07-10 20:33:36 +0530

    localvariable

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,11 +1,12 @@
 //Function -> 2 numbers, sum
 
 function sum(x, y) {
+    //local variables -> scope
     s = x + y;
     console.log("before return");
      return s;
-    console.log("after return");
 }
 
 let val = sum(3, 4);
+console.log(x);
 console.log(val);

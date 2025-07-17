12e14f2d127a9e16bc5e35394e101ea781c37b3d
gunnuwatts <gungunwatts096@gmail.com>
2025-07-17 20:59:19 +0530

    5pracques1a

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,19 +1,5 @@
-//sum function
-function sum(a, b) {
-    return a + b;
+function countVowels(str) {
+    for(const char of str) {
+        console.log(char);
+    }
 }
-
-const arrowSum = (a, b) => {
-    console.log(a + b);
-};
-
-//multiplication function
-function mul(a, b) {
-    return a * b;
-}
-
-const arrowMul = (a, b) => {
-    return a * b;
-}
-
-const printHello = () => console.log("hello");
